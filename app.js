d0a8a14f2cd6bdb0664f3c86f713efa203fa0b82
kharpuds <66372058+kharpuds@users.noreply.github.com>
2020-07-14 22:34:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ var roomCode = null;
 var rooms = [];
 var roomUserMap = new map();
 var userLimitMap = new map();
+var userTurnMap = new map();
 var randomno = 0;
 
 io.on('connection', function (socket) {
 
    socket.on('createRoom', function (data) {
       let users = new Set();
- 
 
       if (!data.username) {
          console.log("Admin Invalid username");
@@ -44,16 +44,15 @@ io.on('connection', function (socket) {
       } else {
          roomno = Math.floor(1000 + Math.random() * 9000);
          userLimitMap.set(roomno, data.limit);
-         socket.id = roomno + data.username;
          rooms.push(roomno);
+         // socket.id=roomno + data.username;
          socket.join(roomno);
          console.log(randomno);
 
          users.add(data.username);  //user who creates joins autoomatically
-         
          roomUserMap.set(roomno, users);
-
-         socket.emit("roomCreated", { description: "Room " + roomno + " created for " + userLimitMap.get(roomno) + " users", socketId: socket.id });
+         userTurnMap.set(roomno,1);
+         socket.emit("roomCreated", { description: "Room " + roomno + " created for " + userLimitMap.get(roomno) + " users", page: "<button onclick=" + "'startGame()'" + ">Start Game</button>", room: roomno, order: 1 });
          console.log("Max no of users for room " + roomno + " is " + userLimitMap.get(roomno));
          console.log("Socket id : " + socket.id);
       }
@@ -73,7 +72,6 @@ io.on('connection', function (socket) {
          console.log("Invalid username");
          socket.emit('errorCreatingRoom', { description: "Invalid user name...Please enter valid username" });
       }
-
       else {
 
          if (roomUserMap.get(roomCode).has(enteredName)) {
@@ -87,11 +85,10 @@ io.on('connection', function (socket) {
          else {
             roomUserMap.get(roomCode).add(enteredName);
             users = Array.from(roomUserMap.get(roomCode));
-            socket.id = roomCode + enteredName;
             socket.join(roomCode);
             console.log(roomUserMap.get(roomCode).size + " Players in room " + roomCode + ": " + users);
             // socket.in(roomCode).emit('joinedRoom', { description: roomUserMap.get(roomCode).size + " Players in room " + roomCode + ": " + users })
-            socket.emit('joinedRoom', { description: roomUserMap.get(roomCode).size + " Players in room " + roomCode + ": " + users, socketId: socket.id });
+            socket.emit('joinedRoom', { description: roomUserMap.get(roomCode).size + " Players in room " + roomCode + ": " + users, page: "<button onclick=" + "'startGame()'" + ">Start Game</button>", room: roomCode, order: roomUserMap.get(roomCode).size });
          }
       }
 
@@ -99,17 +96,39 @@ io.on('connection', function (socket) {
 
 
    socket.on('rollDice', function (data) {
-      var roomnum=Number(socket.id.substr(0,4));
+      // var roomnum=Number(socket.id.substr(0,4));
       var randomnum;
+      var users=[];
+      var roomnum = Number(data.room);
+      var turn=userTurnMap.get(roomnum);
       randomnum = Math.floor(100000 + Math.random() * 900000);
-      console.log(roomnum);
-      console.log(socket.id);
+      users=Array.from(roomUserMap.get(roomnum));
+      console.log(users);
+      if (data.order != turn) {
+         socket.emit('errorDice', { description: "It's not your turn , its currently the turn of player " + users[turn-1] });
+      }
+      else {
+         socket.in(roomnum).emit('sampleOutput', { output: randomnum });
+         socket.emit('sampleOutput', { output: randomnum });
+         console.log(roomnum +"  " +turn);
+         turn++;
+         userTurnMap.set(roomnum,turn);  
+      }
+      // console.log(roomnum);
+      // console.log(socket.id);
       // console.log(randomnum);
-      var roomlength=io.adapter.rooms[roomnum];
-      console.log(Object.keys(roomlength).length);
-      socket.in(roomnum).emit('sampleOutput', { output: randomnum });
-      socket.emit('sampleOutput', { output: randomnum });
+      if(turn>userLimitMap.get(roomnum)){
+         userTurnMap.set(roomnum,1);
+      }
+
    });
+
+   socket.on('startingGame',function(data){
+      console.log("reached starting game");
+      socket.emit('startedGame',{page:"<h1>Transfer Money</h1>\
+      <br />\
+      <button>Transfer</button>"});
+   })
 })
 
 http.listen(3000, function () {
